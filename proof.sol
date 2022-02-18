c0ad309ab4c50875196765e0590ad2dd026f88d8
stephen <akinfolarinstephen8@gmail.com>
2022-02-18 11:01:02 +0100

    update my contract

--- proof.sol
+++ proof.sol
@@ -16,11 +16,15 @@ struct fileDetails{
       _;
       
   }
+  modifier ensureFilesExist(string memory filehash){
+      require(files[filehash].owner !=0,"The file you are requesting for has not being stored on the blockchain");
+      _;
+  }
 
     function storeFiles(string memory fileshash,string memory owner)public ensureOnlyOneFile(fileshash) {
         files[fileshash] = fileDetails(owner,block.timestamp);
     }
-    function getFiles(string memory filehash)public returns(uint,string memory){
+    function getFiles(string memory filehash)public ensureFilesExist(filehash) returns(uint,string memory){
 
         return(files[filehash].timestamp,files[filehash].owner);
 
